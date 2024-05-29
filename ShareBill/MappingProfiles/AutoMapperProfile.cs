42103f54c1e19f281eb00e0860364a8f4fc52761
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-05-29 16:28:44 +1000

    feat: Add AutoMapperProfile for mapping CreateTodoRequest and UpdateTodoRequest to Todo Ignores the Id property of the Todo entity

---
+++ ShareBill/MappingProfiles/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using ShareBill.Contracts;
+using ShareBill.Models;
+
+namespace TodoAPI.MappingProfiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<CreateTodoRequest, Todo>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore());
+
+            CreateMap<UpdateTodoRequest, Todo>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore());
+        }
+    }
+}
