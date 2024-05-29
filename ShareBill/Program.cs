f2ece24864bf1f531d52f12bc50016172ee97970
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-05-29 16:10:11 +1000

    feat: Add ShareBill.AppDataContext and ShareBill.Models
    The commit adds the necessary using statements for `ShareBill.AppDataContext` and `ShareBill.Models` in the `Program.cs` file. This is required for accessing the database context and models in the ShareBill application.

--- ShareBill/Program.cs
+++ ShareBill/Program.cs
@@ -1,3 +1,6 @@
+using ShareBill.AppDataContext;
+using ShareBill.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<DbSettings>(builder.Configuration.GetSection("DbSettings"));
+builder.Services.AddSingleton<TodoDbContext>();
+
 var app = builder.Build();
+
+{
+    using var scope = app.Services.CreateScope();
+    var context = scope.ServiceProvider;
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,6 +27,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseExceptionHandler();
 
 app.UseAuthorization();
 
