e9f949e203270efa178bb0e5add294a11f52d55a
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-05-29 16:26:04 +1000

    chore: Update namespace name

--- ShareBill/Contracts/CreateTodoRequest.cs
+++ ShareBill/Contracts/CreateTodoRequest.cs
@@ -1,17 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 
-public class CreateTodoRequest
+namespace ShareBill.Contracts
 {
-    [Required]
-    [StringLength(100)]
-    public string Title { get; set; }
+    public class CreateTodoRequest
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
 
-    [StringLength(500)]
-    public string Description { get; set; }
+        [StringLength(500)]
+        public string Description { get; set; }
 
-    [Required]
-    public DateTime DueDate { get; set; }
+        [Required]
+        public DateTime DueDate { get; set; }
 
-    [Range(1, 5)]
-    public int Priority { get; set; }
+        [Range(1, 5)]
+        public int Priority { get; set; }
+    }
 }
