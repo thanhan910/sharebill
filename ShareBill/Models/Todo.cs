cc833df839b3eaa9137953d8d0e9c9b79ef47479
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-05-29 17:10:16 +1000

    refactor: Update Todo model properties to remove nullable types

--- ShareBill/Models/Todo.cs
+++ ShareBill/Models/Todo.cs
@@ -6,8 +6,8 @@ namespace ShareBill.Models
     {
         [Key]
         public Guid Id { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
         public bool IsComplete { get; set; }
         public DateTime DueDate { get; set; }
         public int Priority { get; set; }
