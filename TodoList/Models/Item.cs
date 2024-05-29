f0524a7c0de129c641e2de7bdac15432d93687da
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-05-29 23:53:16 +1000

    refactor: Update Todo page component to add functionality for managing payers, owners, and items

---
+++ TodoList/Models/Item.cs
@@ -0,0 +1,10 @@
+namespace TodoList;
+
+public class Item
+{
+    public string? Title { get; set; }
+    public double? Price { get; set; }
+    public double? Quantity { get; set; }
+    public int? PayerId { get; set; }
+    public int? OwnerId { get; set; }
+}
