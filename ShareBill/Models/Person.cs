228abdb9e205c4d44d7ecbd75d87edefd23f2aa9
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-06-01 11:45:20 +1000

    refactor: Update Person class to initialize Name property with an empty string

--- ShareBill/Models/Person.cs
+++ ShareBill/Models/Person.cs
@@ -2,7 +2,7 @@ namespace ShareBill.Models;
 
 public class Person
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public bool IsPayer { get; set; } = true;
     public bool IsOwner { get; set; } = true;
 }
